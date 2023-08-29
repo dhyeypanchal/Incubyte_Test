34b9f2a424d344622616ce04b06fc54091dee9e4
dhyeypanchal <dhyey22403@gmail.com>
2023-08-29 15:36:41 +0530

    left right testing done

--- test/incubyte_test.test.js
+++ test/incubyte_test.test.js
@@ -22,18 +22,37 @@ const forwardbackward = (initial_pos,initial_dir)=>{
 }
 
 
+// now for left and right
+const leftRight = (current_dir,next_dir)=>{
+    const directions = ["N","E","S","W"];
+    const currentIndex = directions.indexOf(current_dir);
+
+    const changeInDirection = next_dir === "left"?-1:1;
+
+    const newIndex = (currentIndex + changeInDirection + 4) %4; 
+    return directions[newIndex];
+}
+
+
 // built test cases below
 describe("Chandrayaan 3 Lunar Craft: Galactic Space Craft Control",()=>{
-    // beforeEach(()=>{
-    //     const initial_pos = [0, 0, 0];
-    //     const initial_dir = "Up";
-    // })
-    test("forward backward check", () => {
-        const initial_pos = [1,5,7];
-        const initial_dir = "Down";
-        const expected_Result = [1,5,6];
 
+    const initial_pos = [1,5,7];
+    const initial_dir = "E";
+    // this is for checking forward and backward
+    test("forward backward check", () => {
+        const expected_Result = [2,5,7];
         let result = forwardbackward(initial_pos, initial_dir);
+        expect(result).toEqual(expected_Result);
+    });
+
+    // this is for checking left and right
+    test("left right check", () => {
+        const initial_dir = "E";
+        const expected_Result = "N";
+        const next_dir = "left"
+
+        let result = leftRight(initial_dir, next_dir);
         expect(result).toEqual(expected_Result);
     });
 });
